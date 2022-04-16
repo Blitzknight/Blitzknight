8dbf7bc20e9981a9396c402e4d0853b916ae7942
ej-aoat <atachi.miko@gmail.com>
2022-04-16 15:21:47 +0900

    PIXI単純なスプライト表示

--- app/Client/Appearance/src/app/app.component.ts
+++ app/Client/Appearance/src/app/app.component.ts
@@ -35,8 +35,43 @@ export class AppComponent implements AfterViewInit {
 
       this.app.stage.addChild(text);
 
+      const container = this.example_Container();
+      // Move container to the center
+      container.x = this.app.screen.width / 2;
+      container.y = this.app.screen.height / 2;
+
+      // Center bunny sprite in local container coordinates
+      container.pivot.x = container.width / 2;
+      container.pivot.y = container.height / 2;
+
+      // Listen for animate update
+      this.app.ticker.add((delta) => {
+        // rotate the container!
+        // use delta to create frame-independent transform
+        container.rotation -= 0.01 * delta;
+      });
+
+      this.app.stage.addChild(container);
+
       PIXI.utils.sayHello('type');
       this.pixiContainer.nativeElement.appendChild(this.app.view);
     }
+  }
+
+  private example_Container(): PIXI.Container {
+    const container = new PIXI.Container();
+
+    const texture = PIXI.Texture.from('assets/bunny.png');
+
+    // Create a 5x5 grid of bunnies
+    for (let i = 0; i < 25; i++) {
+      const bunny = new PIXI.Sprite(texture);
+      bunny.anchor.set(0.5);
+      bunny.x = (i % 5) * 40;
+      bunny.y = Math.floor(i / 5) * 40;
+      container.addChild(bunny);
+    }
+
+    return container;
   }
 }
